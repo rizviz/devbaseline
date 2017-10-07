c0c3bd9ed3e68428ad5b45d195e520c3ccb0b5cd
root <root@bilda-master.router414038.com>
2017-10-07 04:23:37 -0400

    updates and additions of bash profiles

--- baseline_sys.sh
+++ baseline_sys.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Date : 12/1/2014
+# Updated v2: 10/3/2017
+# Original Version Date : 12/1/2014
 # Author : Zeeshan Rizvi / @zeerizvi
 # Purpose : Baseline a development VM/System for development/testing
 # Tasks Performed by this include disabling selinux , disabling firewall, disabling firewall
@@ -8,19 +9,25 @@
 # Apache License: All warranties null & void , use at your own risk
 
 # TODO : Check if its ubunutu or redhat based system & issue install commands accordingly
+# TODO : Check for user is root or not 
+
+# Change to root directory
+cd /root
+
 export PUB_IFACE_NAME=`ip addr | grep -i broadcast | awk '{ print $2 }'| sed 's/:/\ /g' | head -1`
+echo $PUB_IFACE_NAME
 
 # Set ethernet interface to come up always on boot
 sed -i 's/ONBOOT=no/ONBOOT=yes/g' /etc/sysconfig/network-scripts/ifcfg-$PUB_IFACE_NAME 
 
-# Turn the SELINUX Off
+# Turn the SELINUX Off to stop it from mucking around w/ file permissions
 sed -i 's/SELINUX=permissive/SELINUX=disabled/g' /etc/selinux/config
 
-# Turn Firewall Off
+# Turn Firewall Off or Add the ports you want to expose 
 systemctl stop firewalld
 systemctl disable firewalld
 
-# Extra Packages for Enterprise Linux (EPEL), which is a community repository of non-standard 
+# Extra Packages for Enterprise Linux (EPEL),
 # packages for the RHEL & RHEL based distributions
 # This has some interesting packages like ansible. 
 # Install EPEL & Update
@@ -30,13 +37,10 @@ yum -y install epel-release
 yum -y update 
 
 # Install necessary packages 
-yum -y install vim rsync git unzip ansible wget net-tools
+yum -y install vim rsync git unzip ansible wget net-tools bind-utils
 
-# download the rc files or profiles 
-wget https://github.com/r00txs/profiles/archive/master.zip
-unzip master.zip
-cd ~/profiles-master
-./install
+# install the efficient bashrc profile for quick shortcuts. skip / change to suit your needs
+./install_bash_profile.sh
 
 # Before rebooting check w/ user
 read -r -p "Are you sure you want to continue with system reboot? [Y/n]" response
